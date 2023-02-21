3297b1e88c1c8bd1d3f9d6dce64e45d513d9aec1
VasiliyTemniy <flash_er@mail.ru>
2023-02-21 14:26:51 +0300

    fix mongo db uri

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,7 +4,9 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 const MONGODB_URI =
-  process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+  (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'e2e-test')
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
 const SECRET = process.env.SECRET
 
